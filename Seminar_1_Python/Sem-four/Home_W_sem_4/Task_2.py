2c368f14aef3d3c82e734753c9d95ed2698c6387
Ivan <ivanvg86@mail.ru>
2022-07-18 20:58:53 +0300

    add test

--- Seminar_1_Python/Sem-four/Home_W_sem_4/Task_2.py
+++ Seminar_1_Python/Sem-four/Home_W_sem_4/Task_2.py
@@ -1,29 +1,27 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N. 
 
-# import math
-# number = int(input())
-
-# for i in range(2 , int)
 
 
 
 
 
 
-# def factor(n):
-#     my_list = []
-#     d = 2
-#     while d * d <= n:
-#         if n % d == 0:
-#             my_list.append(d)
-#             n //=d
-#         else:
-#             d +=1
-#             if n > 1:
-#                 my_list.append(n)
-#                 return my_list
+
+def factor(n):
+    my_list = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            my_list.append(d)
+            n //=d
+        else:
+            d +=1
+            if n > 1:
+                my_list.append(n)
+                return my_list
 
 
-# print(factor(int(input())))
+print(factor(int(input())))
+
 
 
